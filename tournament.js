39c5570e5af53d8cbf8b60e156f0f7adfad061f7
DeannaMarbeck <deanna.marbeck@gmail.com>
2018-04-03 16:19:15 +0100

    added multiple rounds

--- tournament.js
+++ tournament.js
@@ -5,7 +5,7 @@ let output = d.getElementById("players-list");
 let draw = d.getElementById("draw");
 let drawTitle = d.getElementById("draw-title");
 let roundButton = d.getElementById("round-button");
-let rounds = d.getElementById("rounds");
+let final = d.getElementById("final");
 
 let players = [];
 
@@ -35,12 +35,17 @@ let randomisePlayers = (input, length) => {
 
 // Print the draw
 let printDraw = (players) => {
-
+	let numPlayers = players.length;
+	let matchNum = 1;
 	players.map((player, index) => {
 		if (index % 2 === 0) {
+			let h3 = d.createElement("h3");
+			h3.textContent = "Match " + matchNum;
+			draw.appendChild(h3);
 			p = d.createElement("p");
 			p.textContent = player.name;
 			draw.appendChild(p);
+			matchNum += 1;
 		} else {
 			p = d.createElement("p");
 			p.textContent = " plays "
@@ -49,10 +54,12 @@ let printDraw = (players) => {
 			p.textContent = player.name;
 			draw.appendChild(p);
 		} 
+
 		// Reset player
 		player.isWinner = false;
 	});
-	if (players.length % 2 === 1) {
+
+	if (numPlayers % 2 === 1) {
 		p = d.createElement("p");
 		p.textContent = "has a BYE";
 		draw.appendChild(p);
@@ -62,10 +69,12 @@ let printDraw = (players) => {
 // Create the initial Tournament draw
 let createTournament = () => {
 	let playersCopy = players.slice();
-	let randomisedPlayers = randomisePlayers(playersCopy, players.length);
-	console.log(randomisedPlayers);
-	drawTitle.textContent = "Here is the tournament draw";
-	printDraw(randomisedPlayers);
+	players = randomisePlayers(playersCopy, players.length);
+	let h2 = d.createElement("h2");
+	h2.textContent = "Tournament draw";
+	draw.appendChild(h2);
+	printDraw(players);
+	d.getElementById("add-players").classList.add("hidden");
 }
 
 // Mark a player as the winner
@@ -73,22 +82,35 @@ let winnerClicked = e => {
 	let winner = e.target;
 	winner.style.border = "2px solid red";
 	players.map(player => (player.name == winner.textContent) ? player.isWinner = !player.isWinner : player);
+	if (players.length % 2 === 1) {
+		players[players.length-1].isWinner = true;
+	}
 	console.log(players);
 }
 
 // Generate the next round from the players where isWinner is true
 let roundClicked = () => {
-	let winners = players.filter(player => player.isWinner == true);
+	let winners = players.filter(player => player.isWinner);
+
+// Check winners are selected
+// 	if (winners.length < players.length / 2) {
+// 		d.getElementById("draw-error").textContent = "Please select the winners";
+
+// 	} else 
+
+// Print final if only two players left
 	if (winners.length <= 2) {
+		final.classList.remove("hidden");
 		let p = d.createElement("p");
-		p.textContent = "Last round: " + winners[0].name + " vs " + winners[1].name;
-		draw.appendChild(p);
+		p.textContent = winners[0].name + " vs " + winners[1].name;
+		final.appendChild(p);
 		return;
-	}
-	if (winners.length < players.length / 2) {
-		d.getElementById("draw-error").textContent = "Please select the winners";
+
+// Otherwise generate the next round
 	} else {
-		d.getElementById("round-title").textContent = "Next round";
+		let h2 = d.createElement("h2");
+		h2.textContent = "Next round";
+		draw.appendChild(h2);
 		printDraw(winners);
 	}
 }
